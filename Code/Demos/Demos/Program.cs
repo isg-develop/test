c4fcd9db141155da94b6d4beae06ce31c7644ffa
uth-tics <uth.tics.tareas@gmail.com>
2016-11-15 11:51:20 -0600

    Init demo

---
+++ Code/Demos/Demos/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: elite
+ * Fecha: 15/11/2016
+ * Hora: 11:42 a. m.
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+using System.Windows.Forms;
+
+namespace Demos
+{
+	/// <summary>
+	/// Class with program entry point.
+	/// </summary>
+	internal sealed class Program
+	{
+		/// <summary>
+		/// Program entry point.
+		/// </summary>
+		[STAThread]
+		private static void Main(string[] args)
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new MainForm());
+		}
+		
+	}
+}
