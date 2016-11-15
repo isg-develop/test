c6f8db5e46a7f6c6f49d8b63115b2c9d65614870
uth-tics <uth.tics.tareas@gmail.com>
2016-11-15 13:20:39 -0600

    Models

--- Code/Demos/Demos/MainForm.Designer.cs
+++ Code/Demos/Demos/MainForm.Designer.cs
@@ -14,6 +14,7 @@ namespace Demos
 		/// Designer variable used to keep track of non-visual components.
 		/// </summary>
 		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Label label1;
 		
 		/// <summary>
 		/// Disposes resources used by the form.
@@ -36,12 +37,28 @@ namespace Demos
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(120, 57);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(251, 52);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Bienvenidos";
 			// 
 			// MainForm
 			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Text = "Demos";
+			this.ClientSize = new System.Drawing.Size(492, 182);
+			this.Controls.Add(this.label1);
 			this.Name = "MainForm";
+			this.Text = "Demos";
+			this.ResumeLayout(false);
+
 		}
 	}
 }
---
+++ Code/Demos/Demos/Models/Docente.cs
@@ -0,0 +1,80 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: elite
+ * Fecha: 15/11/2016
+ * Hora: 12:44 p. m.
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+
+namespace Demos.Models
+{
+	/// <summary>
+	/// Description of Docente.
+	/// </summary>
+	public class Docente
+	{
+		private String nombre, apellido, tipo;
+        private  int horas;
+        
+		public Docente(String nombre, String apellido, String tipo, int horas)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.tipo = tipo;
+            this.horas = horas;
+        }
+
+        public String getNombre(){
+            return nombre;
+        }
+        public String getApellido() { 
+            return apellido; 
+        }
+        public String getTipo() { 
+            return tipo; 
+        }
+        public int getHoras() { 
+            return horas; 
+        }
+        public void setHoras(int horas) {
+            this.horas = horas;
+        }
+        public void setNombre(String nombre) { 
+            this.nombre = nombre; 
+        }
+        public void setApellido(String apellido) { 
+            this.apellido = apellido; 
+        }
+        public void setTipo(String tipo) { 
+            this.tipo = tipo;
+        }
+        
+        public String getNombreCompleto()
+        {
+            return nombre + "" + apellido;
+        }
+        public double getSueldoBruto()
+        {
+            if (tipo.Equals("android"))
+                return 3 * horas;
+            else if (tipo.Equals("web"))
+                return 4 * horas;
+            else if (tipo.Equals("desktop"))
+                return 3.5 * horas;
+            else
+                return 5 * horas;
+
+        }
+        public double getDescuento()
+        {
+            return 0.10 * getSueldoBruto();
+        }
+
+        public double getNeto()
+        {
+            return getSueldoBruto() - getDescuento();
+        }
+	}
+}
